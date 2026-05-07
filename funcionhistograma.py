95f9215ab4aa1c8c7e96a2a9f30e0d74469b5cdc
AngelRocca <angelrocca1@gmail.com>
2026-05-07 11:35:24 -0500

    fix: Use float32 for calculating the expansion function

--- funcionhistograma.py
+++ funcionhistograma.py
@@ -3,15 +3,19 @@ import numpy as np
 
 def aplicar_expansion(imagen: np.ndarray) -> np.ndarray:
 
-    r1 = np.percentile(imagen, 1)
-    r2 = np.percentile(imagen, 99)
+    imagen_float = imagen.astype(np.float32)
+
+    r1 = np.percentile(imagen_float, 1)
+    r2 = np.percentile(imagen_float, 99)
 
     if r1 == r2:
         return imagen.copy()
 
-    transformada = ((imagen.astype(np.float32) - r1) / (r2 - r1)) * 255.0
+    transformada = ((imagen_float - r1) / (r2 - r1)) * 255.0
 
-    return np.clip(np.round(transformada), 0, 255).astype(np.uint8)
+    transformada_restringida = np.clip(np.round(transformada), 0, 255)
+
+    return transformada_restringida.astype(np.uint8)
 
 
 def aplicar_ecualizacion(imagen: np.ndarray) -> np.ndarray:
